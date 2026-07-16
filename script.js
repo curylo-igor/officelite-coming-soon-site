879eefab8b6ac118aef4db644bcac0f31fffaec8
curylo-igor <podjeyfa@gmail.com>
2026-07-16 22:01:25 +0200

    dynamic countdowm

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const targetDate = new Date();
-targetDate.setDate(targetDate.getDate() + 45);
+targetDate.setDate(targetDate.getDate() + 30);
 
 const months = [
   "JAN",
@@ -19,3 +19,30 @@ const months = [
 const formattedDate = `${targetDate.getDate()} ${months[targetDate.getMonth()]} ${targetDate.getFullYear()}`;
 
 document.getElementById("target-date").innerText = formattedDate;
+
+const countdownInterval = setInterval(() => {
+  const now = new Date().getTime();
+
+  const distance = targetDate.getTime() - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  const padNumber = (number) => (number < 10 ? `0${number}` : number);
+
+  document.getElementById("days").innerText = padNumber(days);
+  document.getElementById("hours").innerText = padNumber(hours);
+  document.getElementById("minutes").innerText = padNumber(minutes);
+  document.getElementById("seconds").innerText = padNumber(seconds);
+
+  if (distance < 0) {
+    document.getElementById("days").innerText = "00";
+    document.getElementById("hours").innerText = "00";
+    document.getElementById("minutes").innerText = "00";
+    document.getElementById("seconds").innerText = "00";
+  }
+}, 1000);
